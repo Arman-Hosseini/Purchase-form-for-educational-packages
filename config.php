712c48e490eea584a17c9d1ef977e319f016bdac
Arman Hosseini <namra.1377@gmail.com>
2019-05-28 01:25:39 +0430

    Update config.php Added db.php for database config Added PDO connection for connect to database Added a constant for `<br />` tags

--- config.php
+++ config.php
@@ -3,6 +3,25 @@
 	In the name of God
 	Written By: Arman Hosseini
  */
+
+// Database Config
+require_once( "db.php" );
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=".$dbname.";charset=utf8", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    exit( " خطا در برقراری ارتباط با بانک اطلاعاتی: " . $e->getMessage() . "<br />" . PHP_EOL );
+}
+//
+
+// Init fields
+require_once( "init.php" );
+
+// Set head
 $head   = '
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/fonts.css">
@@ -10,14 +29,18 @@ $head   = '
     <script src="assets/js/bootstrap.min.js"></script>
     <style>
     html, body{
-        font-family: Vazir, "tahoma";
+        font-family: Vazir, "tahoma",sans-serif;
         font-size: 15px;
     }
     </style>
 	';
-	
+
+// Set page header
 $header = '
     <div class="page-header">
         <h2>خرید طرح ویژه</h2>      
     </div>
 	';
+
+// Define br tag
+define("br", "<br />" . PHP_EOL);
